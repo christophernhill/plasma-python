26ff23f831e9d4304b689c85a7ce7522438041da
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-03-22 22:57:45 -0400

    Add dependency on tensorflow, bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 #import plasma.version
 
 setup(name = "plasma",
-      version = "0.0.3",
+      version = "0.9.0",
       packages = find_packages(),
       #scripts = [""],
       description = "PPPL deep learning package.",
@@ -17,7 +17,7 @@ setup(name = "plasma",
       download_url = "https://github.com/PPPLDeepLearning/plasma-python",
       #license = "Apache Software License v2",
       test_suite = "tests",
-      install_requires = ['keras','theano','pathos','matplotlib'],
+      install_requires = ['keras','tensorflow','theano','pathos','matplotlib'],
       tests_require = [],
       classifiers = ["Development Status :: 3 - Alpha",
                      "Environment :: Console",
